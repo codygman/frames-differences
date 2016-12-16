2f80a7cc8d638464e75e54e5b3d0dd99b0ce6683
Cody Goodman <codygman.consulting@gmail.com>
2016-12-16 11:38:38 -0600

    add working version

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,105 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import Frames hiding ((:&))
+import Frames.CSV (tableTypesOverride, RowGen(..), rowGen, readTableOpt)
+import Frames.InCore (RecVec)
+import Data.Vinyl (Rec(RNil), RecApplicative(rpure))
+import Data.Vinyl.TypeLevel (RIndex)
+import Control.Lens (view, (&), (?~))
+import Pipes (Producer, (>->), runEffect)
+import Pipes.Internal (Proxy)
+import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe, isNothing)
+import qualified Control.Foldl as L
+import qualified Pipes.Prelude as P
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Map as M
+import qualified Data.Text.Format as T
+
+
+tableTypesOverride rowGen {rowTypeName ="Normalized"} "normalized.csv" [("composite_key", ''Text)]
+tableTypesOverride rowGen {rowTypeName ="Denormalized"} "denormalized.csv" []
+
+mkCompositeKey :: (RecApplicative rs2, KeyA ∈ rs, KeyB ∈ rs, KeyC ∈ rs, KeyD ∈ rs, CompositeKey ∈ rs2) =>
+                  Record rs -> Record rs2
+mkCompositeKey denormRow = fromMaybe (error "failed to make composite key") . recMaybe $ newRow
+  where newRow = rpure Nothing & compositeKey' ?~ Col compositeKeyTxt
+        compositeKeyTxt = view' keyA <> view' keyB <> view' keyC <> view' keyD
+        view' l = LT.toStrict $ T.format "{}" (T.Only (view l denormRow))
+        intToTxt = T.pack . show :: Int -> Text -- slow!
+
+normalized :: Producer Normalized IO ()
+normalized = readTableOpt normalizedParser "normalized.csv"
+-- λ> runEffect $ normalized >-> P.take 5 >-> P.print
+-- {composite_key :-> "1234", tag :-> "one"}
+-- {composite_key :-> "5678", tag :-> "two"}
+-- {composite_key :-> "9101", tag :-> "three"}
+
+denormalized :: Producer Denormalized IO ()
+denormalized = readTableOpt denormalizedParser "denormalized.csv"
+-- λ> runEffect $ denormalized >-> P.take 5 >-> P.print
+-- {key_a :-> 1, key_b :-> 2, key_c :-> 3, key_d :-> 4, tag :-> "one"}
+-- {key_a :-> 5, key_b :-> 6, key_c :-> 7, key_d :-> 8, tag :-> "two"}
+-- {key_a :-> 9, key_b :-> 1, key_c :-> 0, key_d :-> 1, tag :-> "three"}
+
+-- denormalized' is actually the "normalized" denormalized in that it contains the same compositeKey. I was lazy and didn't care about keeping the tag.
+denormalized' :: Producer (Record '[CompositeKey]) IO ()
+denormalized' = denormalized >-> P.map mkCompositeKey
+-- λ> runEffect $ denormalized' >-> P.take 5 >-> P.print
+-- {composite_key :-> "1234"}
+-- {composite_key :-> "5678"}
+-- {composite_key :-> "9101"}
+
+addCompositeKeyToMapFold :: (Num a, CompositeKey ∈ rs) =>
+                            L.Fold (Record rs) (M.Map Text a)
+addCompositeKeyToMapFold = L.Fold (\m r -> addCompositeKeyToMap m r) (M.empty) id
+
+addCompositeKeyToMap
+  :: (Num a, CompositeKey ∈ rs) =>
+     M.Map Text a -> Record rs -> M.Map Text a
+addCompositeKeyToMap m r = M.insert (view compositeKey r) 0 m
+
+buildCompositeKeyMap :: (Foldable f, CompositeKey ∈ rs) =>
+                        f (Record rs) -> M.Map Text Integer
+buildCompositeKeyMap = L.fold addCompositeKeyToMapFold
+
+findMissingRows :: forall
+                   (m :: * -> *)
+                   (rs :: [*])
+                   a'
+                   a
+                   (m1 :: * -> *)
+                   r
+                   (rs1 :: [*]).
+                   ( Monad m1,
+                     CompositeKey ∈ rs,
+                     CompositeKey ∈ rs1,
+                     L.PrimMonad m,
+                     Frames.InCore.RecVec rs
+                   ) =>
+                   Pipes.Internal.Proxy a' a () (Record rs1) m1 r
+                -> Producer (Record rs) m ()
+                -> m (Pipes.Internal.Proxy a' a () (Record rs1) m1 r)
+findMissingRows referenceProducer checkProducer = do
+  -- build the index of rows in the producer to check
+  compositeKeyMap <- buildCompositeKeyMap <$> inCoreAoS checkProducer
+  -- we could have built compositeKeyMap in a single line if we were golfing
+  -- L.fold (L.Fold (\m r -> M.insert (view compositeKey r) 0 m) (M.empty) id) <$> inCoreAoS checkProducer
+
+  -- keep only rows we didn't have in the checkProducer index produced
+  return $ referenceProducer >-> P.filter (\r -> isNothing $ M.lookup (view compositeKey r) compositeKeyMap)
+
+printMissingRows :: IO ()
+printMissingRows = do
+  putStrLn "rows normalized contains deonrmalized does not"
+  findMissingRows normalized denormalized' >>= \p -> runEffect $ p >-> P.print
+
+main = undefined
